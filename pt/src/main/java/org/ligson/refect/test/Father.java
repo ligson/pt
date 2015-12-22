bdbef10aa6e77fa9635e91224533be982601a87f
ligson <593949938@qq.com>
2015-12-22 16:20:03 +0800

    验证反射

---
+++ pt/src/main/java/org/ligson/refect/test/Father.java
@@ -0,0 +1,7 @@
+package org.ligson.refect.test;
+
+public class Father {
+	public void sayname(){
+		System.out.println(this.getClass().getName());
+	}
+}
---
+++ pt/src/main/java/org/ligson/refect/test/Son.java
@@ -0,0 +1,8 @@
+package org.ligson.refect.test;
+
+public class Son extends Father{
+	public static void main(String[] args) {
+		Father son = new Son();
+		son.sayname();
+	}
+}
