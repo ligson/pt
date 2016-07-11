70bae9750cdba6ce0336cf37a560d3db5dc35a5a
lijinsheng <lijinsheng@e-ling.com.cn>
2016-07-11 09:27:54 +0800

    随机选择

--- src/main/java/zk/providers/Consumer.java
+++ src/main/java/zk/providers/Consumer.java
@@ -22,16 +22,22 @@ public class Consumer {
 		this.zkAddress = zkAddress;
 	}
 
+	public String judeServiceNameByRandom(List<String> serviceList){
+		int idx = (int) (Math.random() * serviceList.size());
+		System.out.println("-----------idx"+idx);
+		String serviceNodeName = serviceList.get(idx);
+		return serviceNodeName;
+	}
 	public void start() throws Exception {
 		ZkClient zkClient = new ZkClient(zkAddress);
 		List<String> serviceList = zkClient.getChildren("/provider");
 		if (serviceList.size() == 0) {
 			throw new Exception("provider list is emtpy!");
 		}
-		int idx = (int) (Math.random() * serviceList.size());
-		System.out.println("-----------idx"+idx);
-		String serviceNodeName = serviceList.get(idx);
-		String serviceAddr = zkClient.readData("/provider/" + 3);
+		//随机选择
+		String serviceNodeName = judeServiceNameByRandom(serviceList);
+		
+		String serviceAddr = zkClient.readData("/provider/" + serviceNodeName);
 		String[] addrs = serviceAddr.split(":");
 		System.out.println("connect:"+serviceAddr+":serviceNameL:"+serviceNodeName);
 		Socket socket = new Socket(addrs[0], Integer.parseInt(addrs[1]));
